0fbaa735f5ef61f4e674ea7ec3d7d2a6ee12a780
BeratDgan <dgan.berat@gmail.com>
2026-02-24 15:45:42 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Berat Doğan</p> 
+      <p>Ogrenci No: 235541062</p>
+      <p>Yas: 21</p>
+      <p>Cinsiyet: Erkek</p>
+
+    </div>
+  )
+}
+export default App
