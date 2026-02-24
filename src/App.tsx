f4cbc973a8403b0268a79b0e27f68ab1753e0d29
BeratDgan <dgan.berat@gmail.com>
2026-02-24 15:49:40 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       <h1>Web Tasarimi ve Programlama</h1>
       <h2>LAB-1</h2>
       <p>Ad Soyad: Berat Doğan</p> 
-      <p>Ogrenci No: 235541062</p>
+      <p>Oğrenci No: 235541062</p>
       <p>Yas: 21</p>
       <p>Cinsiyet: Erkek</p>
 
